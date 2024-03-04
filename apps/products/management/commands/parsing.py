d7071d5931d74e1dd4ec7a1c343f0cdc7f483f01
Toktorov <ktoktorov2005@gmail.com>
2024-03-04 18:35:32 +0600

    parser work

--- apps/products/management/commands/parsing.py
+++ apps/products/management/commands/parsing.py
@@ -19,3 +19,11 @@ class Command(BaseCommand):
             print(phone.text)
             result.append({'title':phone.text, 'price':price.text.strip().replace(' ', '').replace('сом', '')})
         print(result)
+        #Начинаем данные которые спарсены закидывать в БД
+        for p in result:
+            product = Product.objects.get_or_create(user_id=1, title=p['title'],
+                                                    description=p['title'],
+                                                    price=p['price'],
+                                                    image='no_image.png',
+                                                    category_id=1)
+        print("Данные успешно добавлены")
